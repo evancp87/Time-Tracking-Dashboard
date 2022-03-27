4da02c878e73dfd5d2f96b76dc990b60ad2e16da
Evan Parker <65721884+evancp87@users.noreply.github.com>
2022-03-27 22:12:21 +0100

    initial js functionality, getting elements and json fetch

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,35 @@
-const daily = document.getElementById('daily');
-const weekly = document.getElementById('weekly');
-const monthly = document.getElementById('monthly');
+// const daily = document.getElementById('daily');
+// const weekly = document.getElementById('weekly');
+// const monthly = document.getElementById('monthly');
+
+const stats = document.querySelectorAll('.stats');
+console.log(stats);
+console.log("hello world");
+
+
+// const getData = async () => {
+// const fetchData =  await fetch(data.json);
+// }
+
+
+const getData = async () => {
+    try {
+        const response = await fetch('data.json');
+        const stats = await response.json();
+
+        return stats;
+        
+    }
+    catch (error) {
+        console.log('There was an error fetching the data');
+        console.log(error);
+    }
+}
+
+console.log(getData());
+
+getData().then(stats => {
+    return stats;
+})
+
+// OOP comparisons between variables and data
